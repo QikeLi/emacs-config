759a0726155ae6461eea0580e12b50fbeb49ced1
QikeLi <qikeli@email.arizona.edu>
2017-01-08 16:33:26 -0700

    set up automatic clock-in and clock-out

--- packages.el
+++ packages.el
@@ -84,7 +84,31 @@
    ;; Do not prompt for code evaluation
    '(org-confirm-babel-evaluate nil))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; clock-in when task is in STARTED state; clock-out when task is in WAITING state.
+  (defun sacha/org-clock-in-if-starting ()
+    "Clock in when the task is marked STARTED."
+    (when (and (string= org-state "STARTED")
+	       (not (string= org-last-state org-state)))
+      (org-clock-in)))
+
+  (add-hook 'org-after-todo-state-change-hook
+	    'sacha/org-clock-in-if-starting)
+
+  (defadvice org-clock-in (after sacha activate)
+    "Set this task's status to 'STARTED'."
+    (org-todo "STARTED"))
+
+  (defun sacha/org-clock-out-if-waiting ()
+    "Clock in when the task is marked STARTED."
+    (when (and (string= org-state "WAITING")
+	       (not (string= org-last-state org-state)))
+      (org-clock-out)))
+
+  (add-hook 'org-after-todo-state-change-hook
+	    'sacha/org-clock-out-if-waiting)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
