5b05c6523aed3f95da699e91b4c02932d555a7c0
QikeLi <qikeli@email.arizona.edu>
2017-01-09 12:20:26 -0700

    turn off one more org-agenda-to-appt; move scimax folder to dropbox

--- user/user.el
+++ user/user.el
@@ -161,7 +161,7 @@
 ;; Turn on agenda reminder
 ;; (org-agenda-to-appt)
 ;; Everyday at 12:05am run org-agenda-to-appt(useful in case you keep Emacs always on)
-(run-at-time "12:05am" (* 24 3600) 'org-agenda-to-appt)
+;; (run-at-time "12:05am" (* 24 3600) 'org-agenda-to-appt)
 ;; prvent start a new frame when open a file from Mac Finder
 (setq ns-pop-up-frames nil)
 
