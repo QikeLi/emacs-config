1c0ea64587183336f0731b12fe1caecd173770a8
QikeLi <qikeli@email.arizona.edu>
2017-01-09 11:22:12 -0700

    Turn off a few setting in user.el to get a stable scimax

--- user/user.el
+++ user/user.el
@@ -163,74 +163,74 @@
 ;; Everyday at 12:05am run org-agenda-to-appt(useful in case you keep Emacs always on)
 ;; (run-at-time "12:05am" (* 24 3600) 'org-agenda-to-appt)
 ;; prvent start a new frame when open a file from Mac Finder
-(setq ns-pop-up-frames nil)
+;; (setq ns-pop-up-frames nil)
 
 ;; ** a function to open file at cursor
-(defun xah-open-file-at-cursor ()
-  "Open the file path under cursor.
-If there is text selection, uses the text selection for path.
-If the path starts with “http://”, open the URL in browser.
-Input path can be {relative, full path, URL}.
-Path may have a trailing “:‹n›” that indicates line number. If so, jump to that line number.
-If path does not have a file extension, automatically try with “.el” for elisp files.
-This command is similar to `find-file-at-point' but without prompting for confirmation.
+;; (defun xah-open-file-at-cursor ()
+;;   "Open the file path under cursor.
+;; If there is text selection, uses the text selection for path.
+;; If the path starts with “http://”, open the URL in browser.
+;; Input path can be {relative, full path, URL}.
+;; Path may have a trailing “:‹n›” that indicates line number. If so, jump to that line number.
+;; If path does not have a file extension, automatically try with “.el” for elisp files.
+;; This command is similar to `find-file-at-point' but without prompting for confirmation.
 
-URL `http://ergoemacs.org/emacs/emacs_open_file_path_fast.html'"
-  (interactive)
-  (let ((-path (if (use-region-p)
-                   (buffer-substring-no-properties (region-beginning) (region-end))
-                 (let (p0 p1 p2)
-                   (setq p0 (point))
-                   ;; chars that are likely to be delimiters of full path, e.g. space, tabs, brakets.
-                   (skip-chars-backward "^  \"\t\n`'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\`")
-                   (setq p1 (point))
-                   (goto-char p0)
-                   (skip-chars-forward "^  \"\t\n`'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\'")
-                   (setq p2 (point))
-                   (goto-char p0)
-                   (buffer-substring-no-properties p1 p2)))))
-    (if (string-match-p "\\`https?://" -path)
-        (browse-url -path)
-      (progn ; not starting “http://”
-        (if (string-match "^\\`\\(.+?\\):\\([0-9]+\\)\\'" -path)
-            (progn
-              (let (
-                    (-fpath (match-string 1 -path))
-                    (-line-num (string-to-number (match-string 2 -path))))
-                (if (file-exists-p -fpath)
-                    (progn
-                      (find-file -fpath)
-                      (goto-char 1)
-                      (forward-line (1- -line-num)))
-                  (progn
-                    (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" -fpath))
-                      (find-file -fpath))))))
-          (progn
-            (if (file-exists-p -path)
-                (find-file -path)
-              (if (file-exists-p (concat -path ".el"))
-                  (find-file (concat -path ".el"))
-                (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" -path))
-                  (find-file -path ))))))))))
+;; URL `http://ergoemacs.org/emacs/emacs_open_file_path_fast.html'"
+;;   (interactive)
+;;   (let ((-path (if (use-region-p)
+;;                    (buffer-substring-no-properties (region-beginning) (region-end))
+;;                  (let (p0 p1 p2)
+;;                    (setq p0 (point))
+;;                    ;; chars that are likely to be delimiters of full path, e.g. space, tabs, brakets.
+;;                    (skip-chars-backward "^  \"\t\n`'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\`")
+;;                    (setq p1 (point))
+;;                    (goto-char p0)
+;;                    (skip-chars-forward "^  \"\t\n`'|()[]{}<>〔〕“”〈〉《》【】〖〗«»‹›·。\\'")
+;;                    (setq p2 (point))
+;;                    (goto-char p0)
+;;                    (buffer-substring-no-properties p1 p2)))))
+;;     (if (string-match-p "\\`https?://" -path)
+;;         (browse-url -path)
+;;       (progn ; not starting “http://”
+;;         (if (string-match "^\\`\\(.+?\\):\\([0-9]+\\)\\'" -path)
+;;             (progn
+;;               (let (
+;;                     (-fpath (match-string 1 -path))
+;;                     (-line-num (string-to-number (match-string 2 -path))))
+;;                 (if (file-exists-p -fpath)
+;;                     (progn
+;;                       (find-file -fpath)
+;;                       (goto-char 1)
+;;                       (forward-line (1- -line-num)))
+;;                   (progn
+;;                     (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" -fpath))
+;;                       (find-file -fpath))))))
+;;           (progn
+;;             (if (file-exists-p -path)
+;;                 (find-file -path)
+;;               (if (file-exists-p (concat -path ".el"))
+;;                   (find-file (concat -path ".el"))
+;;                 (when (y-or-n-p (format "file doesn't exist: 「%s」. Create?" -path))
+;;                   (find-file -path ))))))))))
 
 
-;; ** a function and keybinding to swap window postions
-;; swap two windows
-(defun swap-window-positions ()         ; Stephen Gildea
-  "*Swap the positions of this window and the next one."
-  (interactive)
-  (let ((other-window (next-window (selected-window) 'no-minibuf)))
-    (let ((other-window-buffer (window-buffer other-window))
-	  (other-window-hscroll (window-hscroll other-window))
-	  (other-window-point (window-point other-window))
-	  (other-window-start (window-start other-window)))
-      (set-window-buffer other-window (current-buffer))
-      (set-window-hscroll other-window (window-hscroll (selected-window)))
-      (set-window-point other-window (point))
-      (set-window-start other-window (window-start (selected-window)))
-      (set-window-buffer (selected-window) other-window-buffer)
-      (set-window-hscroll (selected-window) other-window-hscroll)
-      (set-window-point (selected-window) other-window-point)
-      (set-window-start (selected-window) other-window-start))
-    (select-window other-window)))
-(global-set-key (kbd "C-c C-x p") 'swap-window-positions)
+;; ;; ** a function and keybinding to swap window postions
+;; ;; swap two windows
+;; (defun swap-window-positions ()         ; Stephen Gildea
+;;   "*Swap the positions of this window and the next one."
+;;   (interactive)
+;;   (let ((other-window (next-window (selected-window) 'no-minibuf)))
+;;     (let ((other-window-buffer (window-buffer other-window))
+;; 	  (other-window-hscroll (window-hscroll other-window))
+;; 	  (other-window-point (window-point other-window))
+;; 	  (other-window-start (window-start other-window)))
+;;       (set-window-buffer other-window (current-buffer))
+;;       (set-window-hscroll other-window (window-hscroll (selected-window)))
+;;       (set-window-point other-window (point))
+;;       (set-window-start other-window (window-start (selected-window)))
+;;       (set-window-buffer (selected-window) other-window-buffer)
+;;       (set-window-hscroll (selected-window) other-window-hscroll)
+;;       (set-window-point (selected-window) other-window-point)
+;;       (set-window-start (selected-window) other-window-start))
+;;     (select-window other-window)))
+;; (global-set-key (kbd "C-c C-x p") 'swap-window-positions)
