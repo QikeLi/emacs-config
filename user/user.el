7f6b1812731811e293b9231701e7e909b1e3ace5
QikeLi <qikeli@email.arizona.edu>
2017-01-10 01:15:42 -0700

    Fix the issue that flyspell cannot load dictionary

--- user/user.el
+++ user/user.el
@@ -159,10 +159,12 @@
 
 ;; * Miscellaneous
 ;; ** set some variables
+;; Turn on (flyspell-mode)
+(flyspell-mode)				;; Note: force to turn this on when start up emacs since otherwise have trouble to load dictionary. see the issue I raised at scimax: https://github.com/jkitchin/scimax/issues/32
 ;; Turn on agenda reminder
 ;; (org-agenda-to-appt)
 ;; Everyday at 12:05am run org-agenda-to-appt(useful in case you keep Emacs always on)
-;; (run-at-time "12:05am" (* 24 3600) 'org-agenda-to-appt)
+(run-at-time "12:05am" (* 24 3600) 'org-agenda-to-appt)
 ;; prvent start a new frame when open a file from Mac Finder
 (setq ns-pop-up-frames nil)
 ;; move files to ~/.Trash when delete
